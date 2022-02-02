153adc414a847753ae43b2765e29723111d2764b
yaasin-raki2 <yassin23raki@gmail.com>
2022-02-02 15:54:04 +0100

    linking the app to a containerized postgressql database using typeorm and docker-compose, configuring environement variables using .env and the Config module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+	controllers: [AuthController],
+	providers: [AuthService]
+})
+export class AuthModule {}
